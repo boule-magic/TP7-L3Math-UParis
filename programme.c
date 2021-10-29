99e7fe46d71870165a07470fc242a66f5d4ad07b
boule-magic <benoit2montazeaud@gmail.com>
2021-10-29 16:12:17 +0200

    fin

--- programme.c
+++ programme.c
@@ -1,11 +1,16 @@
 #include <gtk/gtk.h>
 #include "graphiques.h"
 #include <math.h>
+#include <complex.h>
+#include <stdio.h>
+
+complex double suite(complex double c, int n);
+int mandelbrot(complex double c);
 
 int
 main()
 {
-    gr_init(800, 400);
+    gr_init(1200, 400);
 
     //Exercice 2
     
@@ -44,9 +49,41 @@ main()
 	    gr_draw_point(800-260+i, 400-260+j, i, j, 0);
 	}
     }
-
-
     
+    //Exercice 6
+
+    int mandelbrot_var;
+    
+    for(double i = 0 ; i < 400 ; i++) {
+	for(double j = 0 ; j < 400 ; j++) {
+	    mandelbrot_var = mandelbrot((i-200)/100+((j-200)/100)*I);
+	    if(mandelbrot_var == -1) {
+	    	gr_draw_point(i+800, j, 0, 0, 0);
+	    }
+	    else {
+		gr_draw_point(i+800, j, mandelbrot_var*10 % 256, 0, mandelbrot_var*10 % 256);
+	    }
+	}
+    }
     gr_display();
+    
     return 0;
+}
+
+complex double suite(complex double c, int n) {
+    if(n > 0) {
+	return suite(c,n-1)*suite(c,n-1) + c;
+    }
+    return 0;
+}
+
+int mandelbrot(complex double c) {
+    complex double z = 0;
+    int n;
+    for(n = 0 ; creal(z) * creal(z) + cimag(z) * cimag(z) < 4 && n < 1000 ; n++) {
+	z = z*z+c;
+    }
+    if(n >= 1000)
+	return -1;
+    return n;
 }
